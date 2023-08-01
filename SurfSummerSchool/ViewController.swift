405cddd7c0a6f44e37803ca0f3742df9e4ff369f
Nikita Shestakov <nikitos3046@yandex.ru>
2023-08-01 13:34:45 +0500

    Initial Commit

---
+++ SurfSummerSchool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurfSummerSchool
+//
+//  Created by Никита Шестаков on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
